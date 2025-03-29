d35b27501e1d2641eeda4d8af778fbe3e3239549
Peter Bjorklund <piot@hotmail.com>
2025-03-29 13:13:01 +0100

    fix: update header

--- crates/wgpu/src/lib.rs
+++ crates/wgpu/src/lib.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/swamp/mireforge
+ * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/mireforge/mireforge
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  */
 use wgpu::{
